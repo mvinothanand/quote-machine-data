dbc88bcc796c3d00707f561b6203f3713c0116f9
mvinothanand <vinoth.anand@gmail.com>
2021-11-26 13:13:43 +0530

    initial commit

---
+++ colors.js
@@ -0,0 +1,13 @@
+// contains a list of colors
+const themeColors = [
+  "#66806a",
+  "#753188",
+  "#2e4c6d",
+  "#181d31",
+  "#533535",
+  "#87aaaa",
+  "#7eb5a6",
+  "#eba83a",
+  "#766161",
+  "#ff8882",
+]
---
+++ quotes.js
@@ -0,0 +1,71 @@
+const quotes = [
+  {
+    id: 1,
+    quote:
+      "Anything that makes you weak physically, intellectually and spiritually, reject as poison",
+    author: "Swami Vivekananda",
+  },
+  {
+    id: 2,
+    quote:
+      "You have to grow from the inside out. No one can teach you, none can make you spiritual. There is no other teacher but your own soul.",
+    author: "Swami Vivekananda",
+  },
+  {
+    id: 3,
+    quote: "Don't just sit there. Do something. The answers will follow.",
+    author: "Mark Manson",
+  },
+  {
+    id: 4,
+    quote:
+      "Life is an endless series of problems. The solution to one problem is merely the creation of another.",
+    author: "Mark Manson",
+  },
+  {
+    id: 5,
+    quote:
+      "Spending money to show people how much money you have is the fastest way to have less money",
+    author: "Morgan Housel",
+  },
+  {
+    id: 6,
+    quote:
+      'The highest form of wealth is the ability to wake up every morning and say, "I can do whatever I want today"',
+    author: "Morgan Housel",
+  },
+  {
+    id: 7,
+    quote:
+      "Without passion, you don't have energy. Without energy, you don't have anything.",
+    author: "Warren Buffett",
+  },
+  {
+    id: 8,
+    quote:
+      "We don't have to be smarter than the rest. We have to be more disciplined than the rest.",
+    author: "Warren Buffett",
+  },
+  {
+    id: 9,
+    quote: "In a world deluged by irrelevant information, clarity is power",
+    author: "Yuval Noah Harari",
+  },
+  {
+    id: 10,
+    quote: "The big money is not in buying or selling. But in the waiting.",
+    author: "Charlie Munger",
+  },
+  {
+    id: 11,
+    quote:
+      "Men, it has been well said, think in herds; it will be seen that they go mad in herds, while they only recover their senses slowly, one by one",
+    author: "Charles Mackay",
+  },
+  {
+    id: 12,
+    quote:
+      "One machine can do the work of fifty ordinary men. No machine can do the work of one extraordinary man.",
+    author: "Elbert Hubbard",
+  },
+]
